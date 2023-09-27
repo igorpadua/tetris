146e79fe6e8d653e1e5040a5117ac07190886549
igorpadua <igorpadua@tutanota.com>
2023-09-27 15:54:01 -0300

    reset

--- tetris.hpp
+++ tetris.hpp
@@ -8,31 +8,5 @@ class Tetris
 {
 public:
     Tetris();
-    void run();
-
-protected:
-    void events();
-    void draw();
-    void moveToDown();
-
-private:
-    std::unique_ptr<sf::RenderWindow> m_window;
-    std::unique_ptr<sf::Sprite> m_sprite;
-    sf::Texture m_tiles;
-
-    static const std::uint32_t LINES = 20;
-    static const std::uint32_t COLS = 10;
-    static const std::uint32_t SQUARES = 4;
-    static const std::uint32_t SHAPES = 7;
-
-    std::vector<std::vector<std::uint32_t>> m_area;
-    std::vector<std::vector<std::uint32_t>> m_forms;
-
-    struct Coords
-    {
-        std::uint32_t x, y;
-    } z[SQUARES], k[SQUARES];
-
-    void closedWindow();
 };
 
